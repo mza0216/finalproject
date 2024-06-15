93d09b4060516a7e26982eb92ffe11a304c890b4
mza0216 <mza0216@auburn.edu>
2024-06-14 21:31:05 -0700

    Initial Commit

---
+++ finalproject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  finalproject
+//
+//  Created by Marc Atienza on 6/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ finalproject/finalprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalprojectApp.swift
+//  finalproject
+//
+//  Created by Marc Atienza on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct finalprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
